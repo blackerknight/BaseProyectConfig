80198a3264ffa590d7ce6df8e404a5210c71c633
eduardo mancilla <eduardo.mancilla@upax.com.mx>
2019-09-04 18:36:59 -0500

    Initial Commit

---
+++ BaseProyectConfig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseProyectConfig
+//
+//  Created by eduardo mancilla on 9/4/19.
+//  Copyright © 2019 black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
